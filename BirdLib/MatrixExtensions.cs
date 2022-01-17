70ffff2a497774b1db3934af366c41ab864ab7b0
Shai Aharoni <saharoni@gmail.com>
2022-01-17 19:45:30 +0200

    Added matrix extensions

---
+++ BirdLib/MatrixExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BirdLib
+{
+    public static class MatrixExtensions
+    {
+        public static T[] GetRow<T>(this T[,] matrix, int rowIndex)
+        {
+            return Enumerable.Range(0, matrix.GetLength(1))
+              .Select(columnIndex => matrix[rowIndex, columnIndex])
+              .ToArray();
+
+        }
+
+        public static T[] GetColumn<T>(this T[,] matrix, int columnIndex)
+        {
+            return Enumerable.Range(0, matrix.GetLength(0))
+              .Select(rowIndex => matrix[rowIndex, columnIndex])
+              .ToArray();
+
+        }
+
+    }
+}
---
+++ BirdLib/MatrixOwl.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BirdLib
+{
+    public class MatrixOwl
+    {
+        public int[,] ParseIntMatrix(string[] lines)
+        {
+            int numOfColumns = lines[0].Length;
+            int numOfRows = lines.Length;
+
+            int[,] map = new int[numOfRows, numOfColumns];
+
+            for (int i = 0; i < numOfRows; i++)
+            {
+                for (int j = 0; j < numOfColumns; j++)
+                {
+                    map[i, j] = int.Parse(lines[i][j].ToString());
+                }
+            }
+
+            return map;
+        }
+    }
+}
