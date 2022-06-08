afd4ec3afcc6dc6b99180698e75ccd1262df387a
Shai Aharoni <saharoni@gmail.com>
2022-06-08 18:34:37 +0300

    Changed to .NET 6 + Added AsyncPuzzleSolver

---
+++ BirdLib/AOC/IAsyncPuzzleSolver.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BirdLib.AOC
+{
+    public interface IAsyncPuzzleSolver
+    {
+        Task<string> SolvePuzzlePart1();
+        Task<string> SolvePuzzlePart2();
+    }
+}
