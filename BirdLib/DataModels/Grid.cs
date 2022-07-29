99dce30bbfb01ee513393d7a165132587b2e615a
Shai Aharoni <saharoni@gmail.com>
2022-07-29 10:35:23 +0300

    Several updates

---
+++ BirdLib/DataModels/Grid.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BirdLib.DataModels
+{
+    public class Grid<T>
+    {
+        private T[,] _items;
+
+        public int RowSize => _items.GetLength(0);
+        public int ColumnSize => _items.GetLength(1);
+
+        public Grid(int rowSize, int columnSize)
+        {
+            _items = new T[rowSize, columnSize];
+        }
+
+        public void SetItem(T item, int rowIndex, int columnIndex)
+        {
+            _items[rowIndex, columnIndex] = item;
+        }
+
+        public T GetItem(int rowIndex, int columnIndex)
+        {
+            return _items[rowIndex, columnIndex];
+        }
+   
+     }
+}
---
+++ BirdLib/GridEagle.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BirdLib
+{
+    public class GridEagle
+    {
+        public static List<Point> GetAllNeighbourPointsFor(int x, int y, int gridSize)
+        {
+            var neighbourPoints = new List<Point>();
+
+            if (x > 0)
+            {
+                neighbourPoints.Add(new Point(x - 1, y));
+
+            } 
+
+            if (x < (gridSize - 1))
+            {
+                neighbourPoints.Add(new Point(x + 1, y));
+            } 
+
+            if (y > 0)
+            {
+                neighbourPoints.Add(new Point(x, y - 1));
+            } 
+
+            if (y < (gridSize - 1))
+            {
+                neighbourPoints.Add(new Point(x, y + 1));
+            }
+
+            return neighbourPoints;
+        }
+    }
+}
