faa98fb42a5c10ead72b92db414e86da2deb3ba5
Shai Aharoni <saharoni@gmail.com>
2022-10-13 09:12:50 +0300

    Added direction enum

---
+++ BirdLib/Enums/Direction.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BirdLib.Enums
+{
+    public enum Direction
+    {
+        Right = 0,
+        Up = 1,
+        Left = 2,
+        Down = 3
+    }
+
+}
