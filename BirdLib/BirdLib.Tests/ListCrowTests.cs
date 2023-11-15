42cd2c086a2c86c827def118eb955e4490ac1441
Shai Aharoni <saharoni@gmail.com>
2023-11-15 15:07:34 +0200

    Added non-decreasing int array check method

---
+++ BirdLib/BirdLib.Tests/ListCrowTests.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BirdLib.Tests
+{
+    [TestClass]
+    public class ListCrowTests
+    {
+
+        [TestMethod]
+        public void IsNonDecreasingSeriesTest1()
+        {
+            var series = new int[] { 1, 1, 1, 1 };
+            Assert.IsTrue(ListCrow.IsNonDecreasingSeries(series));
+        }
+
+
+        [TestMethod]
+        public void IsNonDecreasingSeriesTest2()
+        {
+            var series = new int[] { 1, 1, 1, 0 };
+            Assert.IsFalse(ListCrow.IsNonDecreasingSeries(series));
+        }
+
+
+        [TestMethod]
+        public void IsNonDecreasingSeriesTest3()
+        {
+            var series = new int[] { 1, 2, 4, 8 };
+            Assert.IsTrue(ListCrow.IsNonDecreasingSeries(series));
+        }
+
+        [TestMethod]
+        public void IsNonDecreasingSeriesTest4()
+        {
+            var series = new int[] { 1, 2, 1, 1 };
+            Assert.IsFalse(ListCrow.IsNonDecreasingSeries(series));
+        }
+    }
+}
--- BirdLib/ListCrow.cs
+++ BirdLib/ListCrow.cs
@@ -6,8 +6,17 @@ using System.Threading.Tasks;
 
 namespace BirdLib
 {
+    /// <summary>
+    /// Some useful utility methods that work with lists
+    /// </summary>
     public static class ListCrow
     {
+        /// <summary>
+        /// Generates integers sequence within a range
+        /// </summary>
+        /// <param name="start"></param>
+        /// <param name="end"></param>
+        /// <returns></returns>
         public static List<int> GenerateIntegerSequence(int start, int end)
         {
             var list = new List<int>();
@@ -18,6 +27,23 @@ namespace BirdLib
             }
 
             return list;
+        }
+
+        /// <summary>
+        /// Returns true if the given integer array is sorted in non-decreasing order
+        /// </summary>
+        /// <param name="series"></param>
+        /// <returns></returns>
+        public static bool IsNonDecreasingSeries(int[] series)
+        {
+            for (int i = 0; i < series.Length - 1; i++)
+            {
+                if (series[i+1] < series[i])
+                {
+                    return false;
+                }
+            }
+            return true;
         }
     }
 }
