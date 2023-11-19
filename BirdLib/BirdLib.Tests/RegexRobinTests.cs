6abef9452178d4f9fb33c5dc6b27ea700355a2ec
Shai Aharoni <saharoni@gmail.com>
2023-11-19 16:24:08 +0200

    Exactly  two consecutive matching chars

---
+++ BirdLib/BirdLib.Tests/RegexRobinTests.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace BirdLib.Tests
+{
+    [TestClass]
+    public class RegexRobinTests
+    {
+        [TestMethod]
+        public void MatchingConsecutiveCharsTest1()
+        {
+            MatchCollection matchCollection = RegexRobin.FindMatchingConsecutiveCharacters("12347844");
+            Assert.IsTrue(matchCollection.Count == 1);
+        }
+
+        [TestMethod]
+        public void HasExactlyTwoMatchingConsecutiveCharactersTest1()
+        {
+           bool result = RegexRobin.HasExactlyTwoMatchingConsecutiveCharacters("123478444");
+           Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void HasExactlyTwoMatchingConsecutiveCharactersTest2()
+        {
+            bool result = RegexRobin.HasExactlyTwoMatchingConsecutiveCharacters("12347844");
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void HasExactlyTwoMatchingConsecutiveCharactersTest3()
+        {
+            bool result = RegexRobin.HasExactlyTwoMatchingConsecutiveCharacters("177778");
+            Assert.IsFalse(result);
+        }
+
+
+        [TestMethod]
+        public void HasExactlyTwoMatchingConsecutiveCharactersTest4()
+        {
+            bool result = RegexRobin.HasExactlyTwoMatchingConsecutiveCharacters("1233478");
+            Assert.IsTrue(result);
+        }
+
+
+    }
+}
--- BirdLib/RegexRobin.cs
+++ BirdLib/RegexRobin.cs
@@ -13,5 +13,23 @@ namespace BirdLib
         {
             return Regex.Matches(input, @"([a-zA-Z0-9])\1");
         }
+
+        public static bool HasExactlyTwoMatchingConsecutiveCharacters(string input)
+        {
+           MatchCollection matchCollection = FindMatchingConsecutiveCharacters(input);
+
+            if (!matchCollection.Any()) return false;
+
+            foreach (Match match in matchCollection)
+            {
+                if ((match.Index == input.Length - 2 || input[match.Index] != input[match.Index + 2]) &&
+                    (match.Index < 2 || input[match.Index] != input[match.Index -2]))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
     }
 }
